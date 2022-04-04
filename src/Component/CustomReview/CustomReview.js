407babf4ecf412daf8706a0118349bb60c87a184
hamidhosen42 <mdhamidhosen8444@gmail.com>
2022-04-04 20:48:31 +0600

    customer review

--- src/Component/CustomReview/CustomReview.js
+++ src/Component/CustomReview/CustomReview.js
@@ -2,22 +2,27 @@ import React from 'react';
 import useReviews from '../../Hook/useReviews';
 import ReviewCart from '../ReviewCart/ReviewCart';
 import './CustomReview.css';
+import { Outlet, Link } from "react-router-dom";
 
 const CustomReview = () => {
   const [reviews, setReview] = useReviews();
     return (
       <div>
-        <h1 className="text-3xl font-bold mb-10 flex justify-center">Customer Reviews(3)</h1>
+        <h1 className="text-3xl font-bold mb-10 flex justify-center">
+          Customer Reviews(3)
+        </h1>
 
-        <div className="flex gap-10 columns-3">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center">
           {reviews.slice(0, 3).map((review) => (
             <ReviewCart review={review} key={review.key}></ReviewCart>
           ))}
         </div>
 
-        <button className="mt-10 bg-indigo-600 rounded py-2 font-bold text-cyan-50 px-20">
-          See All Reviews
-        </button>
+        <Link to="/" className="flex justify-center">
+          <button className="mt-10 bg-indigo-600 rounded py-2 font-bold text-cyan-50 px-20">
+            See All Reviews
+          </button>
+        </Link>
       </div>
     );
 };
--- src/Component/ReviewCart/ReviewCart.js
+++ src/Component/ReviewCart/ReviewCart.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import Rating from "react-rating";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ReviewCart = (props) => {
     const{name,img,title,rating}=props.review;
     return (
       <div className="shadow-lg rounded-2xl w-[250px] bg-white p-4">
-        <h1>{name}</h1>
+        <div>
+          <img src={img} alt="" srcset="" />
+        </div>
+        <h2 className="text-xl font-bold flex justify-center m-5">
+          Name : {name}
+        </h2>
+        <p className="flex justify-center mt-5 mb-5">{title}</p>
+        <Rating
+          initialRating={rating}
+          emptySymbol={<FontAwesomeIcon icon={faStar} />}
+          fullSymbol={
+            <FontAwesomeIcon style={{ color: "goldenrod" }} icon={faStar} />
+          }
+          readonly
+        ></Rating>
+        ;
       </div>
     );
 };
